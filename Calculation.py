6b588b9d52bb3c88d97f653268e61bd4364f7826
めだころ <medakakoro2010@gmail.com>
2024-11-06 01:29:09 +0900

    Update Calculation.py

--- Calculation.py
+++ Calculation.py
@@ -7,7 +7,7 @@ count = -1
 #確率0.6%
 input_chance = 6
 #凸回数
-input_powerup = 0
+input_powerup = input("凸回数")
 print("カウント:リセット完了 確率:",input_chance/1000,"[%]",input_powerup,"[凸]")
 print("計算を開始します")
 
@@ -39,14 +39,14 @@ while input_powerup >= 0:
 
             if (result <= 51) and (last_result == False):
                 #すり抜け
-                print("\033[44m[!X]",input_chance/1000,"[%]が当選がすり抜けた")
+                print("\033[44m[!X]",input_chance/1000,"[%]が当選がすり抜けた\033[49m")
                 #回数加算
                 count_passing += 1
                 #すり抜けが発生したため次回は必ず当選
                 last_result = True
 
             else:
-                print("\033[41m[!]",input_chance/1000,"[%]が当選")
+                print("\033[41m[!]",input_chance/1000,"[%]が当選\033[49m")
                 count_correct += 1
                 input_powerup -= 1
                 #当選したため次回不明
@@ -63,14 +63,14 @@ while input_powerup >= 0:
 
                 if (result <= 51) and (last_result == False):
                     #すり抜け
-                    print("\033[42m[T!X]",input_chance/1000,"[%]が90天井で当選がすり抜けた")
+                    print("\033[42m[T!X]",input_chance/1000,"[%]が90天井で当選がすり抜けた\033[49m")
                     #回数加算
                     count_passing += 1
                     #すり抜けが発生したため次回は必ず当選
                     last_result = True
 
                 else:
-                    print("\033[43m[T!]",input_chance/1000,"[%]が90天井で当選")
+                    print("\033[43m[T!]",input_chance/1000,"[%]が90天井で当選\033[49m")
                     count_correct += 1
                     input_powerup -= 1
                     #当選したため次回不明
@@ -79,14 +79,14 @@ while input_powerup >= 0:
             #天井ではない
             else:
                 #未当選
-                print("\033[40m[X] 未当選")
+                print("\033[40m[X] 未当選\033[49m")
                 count_failure += 1
                 #残天井を減算
                 roof += 1
 
-print("\033[49m集計終了 | 必要ガチャ回数:",count,"回 10連換算:",count/10,"回 すり抜け回数:",count_passing,"回 当選回数:",count_correct,"回 天井回数:",count_roof,"回")
+print("\033[49m集計終了 | 必要ガチャ回数:",count,"回 10連換算:",count/10,"回 すり抜け回数:",count_passing,"回 当選回数:",count_correct,"回 天井回数:",count_roof,"回\033[49m")
 #ポリクローム(モノクローム計算) 1回あたり160ポリクローム必要
 need_polychrome = 160 * count
 #必要課金額(12000円で6480ポリクローム)
 need_money = 12000 * math.ceil(need_polychrome/6480)
-print("必要ポリクローム:約",need_polychrome,"個 必要課金額:約¥",need_money," 課金回数:約",math.ceil(need_polychrome/6480),"回")
+print("必要ポリクローム:約",need_polychrome,"個 必要課金額:約¥",need_money," 課金回数:約",math.ceil(need_polychrome/6480),"回\033[49m")
