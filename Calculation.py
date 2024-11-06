5b2d831a7f03e90e05a43a4d687c84e87268938e
めだころ <medakakoro2010@gmail.com>
2024-11-06 17:09:57 +0900

    Update Calculation.py

--- Calculation.py
+++ Calculation.py
@@ -9,10 +9,25 @@ count = -1
 input_chance = 6
 #凸回数
 input_powerup = int(input("凸回数"))
+
 if input_powerup < 0 or input_powerup > 6:
     print("エラー!:不明な凸回数:",input_powerup)
     sys.exit()
-print("カウント:リセット完了 確率:",input_chance/1000,"[%]",input_powerup,"[凸]")
+#何回シュミレートするか
+input_count_simulate = int(input("何回繰り返しますか?"))
+
+#-1回ってなんやねん
+if input_count_simulate < 0:
+    print("エラー!:不明な凸回数:",input_powerup)
+    sys.exit()
+
+#空白なら1回として
+elif input_count_simulate == "" or input_count_simulate == 1:
+    print("1回としてカウントします")
+    input_count_simulate = 1
+    bool_average = False
+
+print("カウント:リセット完了 確率:",input_chance/1000,"[%]",input_powerup,"[凸]","回数:",input_count_simulate)
 print("計算を開始します")
 
 #宣言
@@ -24,9 +39,14 @@ count_roof = 0
 last_result = False
 need_polychrome = 0
 need_money = 0
+count_simulate = input_count_simulate
+count_powerup = input_powerup
 
-#凸の数だけ繰り返す
-while input_powerup >= 0:
+
+#繰り返し処理
+while count_simulate > 0:
+    #凸の数だけ繰り返す
+    while input_powerup >= 0:
         #回数増加
         count += 1
         #1000(0.1%単位で計算)
@@ -87,6 +107,11 @@ while input_powerup >= 0:
                 count_failure += 1
                 #残天井を減算
                 roof += 1
+    
+    #凸回数を戻す
+    input_powerup = count_powerup
+    count_simulate -= 1
+    print(count_simulate)
 
 print("\033[49m集計終了 | 必要ガチャ回数:",count,"回 10連換算:",count/10,"回 すり抜け回数:",count_passing,"回 当選回数:",count_correct,"回 天井回数:",count_roof,"回\033[49m")
 #ポリクローム(モノクローム計算) 1回あたり160ポリクローム必要
@@ -94,3 +119,11 @@ need_polychrome = 160 * count
 #必要課金額(12000円で6480ポリクローム)
 need_money = 12000 * math.ceil(need_polychrome/6480)
 print("必要ポリクローム:約",need_polychrome,"個 必要課金額:約¥",need_money," 課金回数:約",math.ceil(need_polychrome/6480),"回\033[49m")
+
+#繰り返し処理
+if input_count_simulate >= 1:
+    #平均
+    print("=====================AVERAGE==================")
+    print("\033[49m集計終了 試行回数:",input_count_simulate,"| 平均必要ガチャ回数:",count/input_count_simulate,"回 10連換算:",count/10/input_count_simulate,"回 すり抜け回数:",count_passing/input_count_simulate,"回 当選回数:",count_correct/input_count_simulate,"回 天井回数:",count_roof/input_count_simulate,"回\033[49m")
+    print("必要ポリクローム:約",need_polychrome/input_count_simulate,"個 必要課金額:約¥",need_money/input_count_simulate," 課金回数:約",math.ceil(need_polychrome/6480/input_count_simulate),"回\033[49m")
+
