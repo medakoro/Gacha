bd12951e83e7adc022e71bd976728801a44b12b8
めだころ <medakakoro2010@gmail.com>
2024-11-06 17:21:20 +0900

    Update Calculation.py

--- Calculation.py
+++ Calculation.py
@@ -27,8 +27,14 @@ elif input_count_simulate == "" or input_count_simulate == 1:
     input_count_simulate = 1
     bool_average = False
 
-print("カウント:リセット完了 確率:",input_chance/1000,"[%]",input_powerup,"[凸]","回数:",input_count_simulate)
+#必要時間(予想)
+need_time = input_count_simulate * 0.1
+
+print("カウント:リセット完了 確率:",input_chance/1000,"[%]",input_powerup,"[凸]","回数:",input_count_simulate,"予想必要時間:",need_time,"[sec]")
 print("計算を開始します")
+
+#Enterキー待機
+input("Press Enter key")
 
 #宣言
 count_passing = 0
@@ -124,6 +130,6 @@ print("必要ポリクローム:約",need_polychrome,"個 必要課金額:約¥"
 if input_count_simulate >= 1:
     #平均
     print("=====================AVERAGE==================")
-    print("\033[49m集計終了 試行回数:",input_count_simulate,"| 平均必要ガチャ回数:",count/input_count_simulate,"回 10連換算:",count/10/input_count_simulate,"回 すり抜け回数:",count_passing/input_count_simulate,"回 当選回数:",count_correct/input_count_simulate,"回 天井回数:",count_roof/input_count_simulate,"回\033[49m")
-    print("必要ポリクローム:約",need_polychrome/input_count_simulate,"個 必要課金額:約¥",need_money/input_count_simulate," 課金回数:約",math.ceil(need_polychrome/6480/input_count_simulate),"回\033[49m")
+    print("\033[49m集計終了 試行回数:",input_count_simulate,"| 平均必要ガチャ回数:",count/input_count_simulate,"回 10連換算:",count/10/input_count_simulate,"回 平均すり抜け回数:",count_passing/input_count_simulate,"回 平均当選回数:",count_correct/input_count_simulate,"回 平均天井回数:",count_roof/input_count_simulate,"回\033[49m")
+    print("平均必要ポリクローム:約",need_polychrome/input_count_simulate,"個 平均必要課金額:約¥",need_money/input_count_simulate," 平均課金回数:約",math.ceil(need_polychrome/6480/input_count_simulate),"回\033[49m")
 
